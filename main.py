4234a7179ba5e8b09c3ffc9dffec328848b91f74
Kevin Turcios <turcioskevinr@gmail.com>
2025-02-24 23:17:29 -0800

    passthrough

--- main.py
+++ main.py
@@ -4,35 +4,40 @@ from rich.progress import track
 from pathlib import Path
 from rich import print
 from functools import partial
+from rework.utils import (
+    temporary_directory_change,
+    run_command_in_subprocess,
+    MS_WINDOWS,
+)
 
 
-class Benchmark:
-    def __init__(self, benchmark: Path):
-        self.benchmark = benchmark
-        self.name = benchmark.name
-        self.iterations = 5
+# class Benchmark:
+#     def __init__(self, benchmark: Path):
+#         self.benchmark = benchmark
+#         self.name = benchmark.name
+#         self.iterations = 5
 
-    def run(self): ...
+#     def run(self): ...
 
-    def run_benchmark(self):
-        partialed_commands = {
-            # "Nuitka": partial(
-            #     self.venv.run_command_in_terminal,
-            #     Path(os.getcwd()) / "run_benchmark.cmd",
-            # ),
-            "CPython": partial(
-                self.venv.run_command_with_venv_python, "run_benchmark.py"
-            ),
-        }
+#     def run_benchmark(self):
+#         partialed_commands = {
+#             # "Nuitka": partial(
+#             #     self.venv.run_command_in_terminal,
+#             #     Path(os.getcwd()) / "run_benchmark.cmd",
+#             # ),
+#             "CPython": partial(
+#                 self.venv.run_command_with_venv_python, "run_benchmark.py"
+#             ),
+#         }
 
-        for type, command in partialed_commands.items():
-            print(f"Running benchmark with {type}")
-            for _ in track(
-                range(self.iterations),
-                description=f"Running {type} benchmark",
-                total=self.iterations,
-            ):
-                command()
+#         for type, command in partialed_commands.items():
+#             print(f"Running benchmark with {type}")
+#             for _ in track(
+#                 range(self.iterations),
+#                 description=f"Running {type} benchmark",
+#                 total=self.iterations,
+#             ):
+#                 command()
 
 
 def main():
@@ -47,13 +52,11 @@ def main():
         fname = f"{benchmark.parent.name}/{benchmark.name}"
         console.rule(f"Compiling {benchmark.name} @ {fname}")
         compile_benchmark(benchmark)
-        # run_benchmark(benchmark)
+        run_benchmark(benchmark)
         cleanup(
             benchmark,
             to_keep=[file for file in benchmark.iterdir() if "uv" not in file.name],
         )
-        # benchmark = Benchmark(benchmark=benchmark)
-        # benchmark.run_benchmark()
 
 
 if __name__ == "__main__":
--- rework/tvenv.py
+++ rework/tvenv.py
@@ -8,61 +8,47 @@ from rework.utils import (
 from rework.benchmark_prepare import prepare_benchmark_file
 
 
-class Requirements:
-    def __init__(self, requirements_path: Path):
-        self.path = requirements_path
-
-    def uv_sync_list(self) -> list[str]:
-        requirements = []
-        prereqs = [
-            "wheel",
-            "setuptools",
-            "git+https://github.com/KRRT7/Nuitka@thin-flto",
-            # "nuitka",
-        ]
-        for prereq in prereqs:
-            requirements.extend(["--with", prereq])
-        if not self.path.exists():
-            return requirements
-
-        requirements.extend(["--with-requirements", self.path.as_posix()])
-        return requirements
-
-
 def compile_benchmark(benchmark_path: Path) -> None:
-    requirements = Requirements(benchmark_path / "requirements.txt").uv_sync_list()
+    requirements = Path(benchmark_path / "requirements.txt")
+    reqs_exist = requirements.exists()
     run_benchmark_path = benchmark_path / "run_benchmark.py"
     original_contents = run_benchmark_path.read_text()
     prepare_benchmark_file(benchmark_path)
     with temporary_directory_change(benchmark_path):
-        build_command = [
-            "uv",
-            "run",
-        ]
+        run_command_in_subprocess(["uv", "venv"])
 
-        build_command.extend(requirements)
+        run_command_in_subprocess(
+            [
+                "uv",
+                "pip",
+                "install",
+                "wheel",
+                "setuptools",
+                "git+https://github.com/KRRT7/Nuitka@thin-flto",
+            ]
+        )
+        if reqs_exist:
+            run_command_in_subprocess(
+                ["uv", "pip", "install", "-r", "requirements.txt"]
+            )
 
-        build_command += [
-            "--",
-            "python",
-            "-m",
+        command = ["uvx"]
+        if reqs_exist:
+            command += ["--with-requirements", requirements.as_posix()]
+            command += ["--with", "setuptools", "--with", "wheel"]
+
+        command += [
             "nuitka",
             "--onefile",
-            # "--pgo-c", #PGO is currently broken, open issue on Nuitka
-            # "--pgo-python",
             "--lto=yes",
             "--remove-output",
             "--assume-yes-for-downloads",
             "--clang",
-            "--disable-cache=all",
-            # "--low-memory", # only needed in GH actions
+            # "--disable-cache=all",
             "--run",
+            "run_benchmark.py",
         ]
-        if MS_WINDOWS:
-            build_command.append("--mingw64")
-        build_command.append("run_benchmark.py")
-
-        result = run_command_in_subprocess(build_command)
+        result = run_command_in_subprocess(command)
         if result.returncode != 0:
             raise RuntimeError(f"Failed to compile benchmark: {result.stderr}")
     with run_benchmark_path.open("w") as f:
@@ -77,14 +63,8 @@ def run_benchmark(benchmark_path: Path, iterations: int = 5) -> None:
             "--warmup",
             "10",
         ]
-        if (benchmark_path / "requirements.txt").exists():
-            command += [
-                "--setup",
-                "uv venv && uv pip install -r requirements.txt",
-            ]
-        command.append("uv run script.py")
         command.append("run_benchmark.exe" if MS_WINDOWS else "./run_benchmark.bin")
-        command.append("uv run run_benchmark.py")
+        command.append(".venv/bin/python run_benchmark.py")
         result = run_command_in_subprocess(command)
         if result.returncode != 0:
             raise RuntimeError(f"Failed to run benchmark: {result.stderr}")
